1a71b4362eef02d8dcfb2ef50dfb6ef7f05d9ff1
bfaires <bfaires@adaptivebiotech.com>
2019-04-29 16:39:37 -0700

    Organizational changes; moved curTime to a variable passed by ref in Init(). Removed targetPalette[].

--- PaletteManager.cpp
+++ PaletteManager.cpp
@@ -1,34 +1,52 @@
 #include "PaletteManager.h"
 
+/// Object management
 PaletteManager::PaletteManager() { }
+void PaletteManager::Init(uint32_t* _curTime, uint32_t initialWalkLength, uint32_t intialPauseLength, uint8_t initialPalette) {
+  curTime = _curTime;
+  walkLength = initialWalkLength;
+  pauseLength = intialPauseLength;
+  lastSwitchTime = *curTime;
+  memcpy(palette, allPalettes[0], sizeof(CHSV)*PALETTE_SIZE);
+  memcpy(oldPalette, allPalettes[0], sizeof(CHSV)*PALETTE_SIZE);
+  target = initialPalette % NUM_PALETTES;
+}
+void PaletteManager::SkipTime(uint32_t amount) {
+  lastSwitchTime += amount;
+}
 
+/// Accessors
 uint8_t PaletteManager::GetTarget() { return target; }
-void PaletteManager::SetTarget(uint8_t newTarget, uint32_t curTime) {
+void PaletteManager::SetTarget(uint8_t newTarget) {
   target = newTarget % NUM_PALETTES;
-  memcpy(oldPalette, palette, sizeof(CHSV)*PALETTE_SIZE);
-  memcpy(targetPalette, allPalettes[target], sizeof(CHSV)*PALETTE_SIZE);
-  lastSwitchTime = curTime;
+  
+  if(*curTime - lastSwitchTime > pauseLength) {
+    // Already blending. Reset target and restart blending.
+	memcpy(oldPalette, palette, sizeof(CHSV)*PALETTE_SIZE);
+	lastSwitchTime = *curTime - pauseLength;
+  }
 }
 
 uint32_t PaletteManager::GetWalkLength() { return walkLength; }
-void PaletteManager::SetWalkLength(uint32_t newWalkLength, uint32_t curTime) {
+void PaletteManager::SetWalkLength(uint32_t newWalkLength) {
   walkLength = newWalkLength;
-  memcpy(oldPalette, palette, sizeof(CHSV)*PALETTE_SIZE);
-  lastSwitchTime = curTime;
+  
+  if(*curTime - lastSwitchTime > pauseLength) {
+	// Already blending. Reset target and restart blending. // todo: calc the % of blend completed and adjust lastSwitchTime to match it
+	memcpy(oldPalette, palette, sizeof(CHSV)*PALETTE_SIZE);
+	lastSwitchTime = *curTime - pauseLength;
+  }
 }
 
 uint32_t PaletteManager::GetPauseLength() { return pauseLength; }
-void PaletteManager::SetPauseLength(uint32_t newPauseLength, uint32_t curTime) {
+void PaletteManager::SetPauseLength(uint32_t newPauseLength) {
   memcpy(oldPalette, palette, sizeof(CHSV)*PALETTE_SIZE);
   
-  if(curTime - lastSwitchTime <= pauseLength) {
+  if(*curTime - lastSwitchTime <= pauseLength) {
     // Haven't started blending yet
-    if(curTime - lastSwitchTime <= newPauseLength) {
-      // Just update the timer, neither one has tripped yet
-    }
-    else {
-      // Start blending immediately
-      lastSwitchTime - curTime - newPauseLength;
+    if(*curTime - lastSwitchTime > newPauseLength) {
+      // Start blending immediately; but start at 0
+      lastSwitchTime - *curTime - newPauseLength;
     }
   }
   else {
@@ -39,54 +57,33 @@ void PaletteManager::SetPauseLength(uint32_t newPauseLength, uint32_t curTime) {
   pauseLength = newPauseLength;
 }
 
-void PaletteManager::Init(uint32_t initialWalkLength, uint32_t intialPauseLength, uint32_t curTime) {
-  walkLength = initialWalkLength;
-  pauseLength = intialPauseLength;
-  lastSwitchTime = curTime;
-  memcpy(palette, allPalettes[0], sizeof(CHSV)*PALETTE_SIZE);
-  memcpy(oldPalette, allPalettes[0], sizeof(CHSV)*PALETTE_SIZE);
-  target = 1 % NUM_PALETTES;
-  memcpy(targetPalette, allPalettes[target], sizeof(CHSV)*PALETTE_SIZE);
-}
-
-void PaletteManager::SkipTime(uint32_t amount) {
-  lastSwitchTime += amount;
-}
-
-void PaletteManager::Update(uint32_t curTime) {
-  if(curTime - lastSwitchTime >= pauseLength) {
-    uint32_t transitionTime = curTime - lastSwitchTime - pauseLength;
-    if(transitionTime < walkLength) {
-      uint8_t blendAmount = 255 * transitionTime / walkLength;//debug: changed from 256, why?
-      for(uint8_t i = 0; i < PALETTE_SIZE; i++) {
-        palette[i] = blend(oldPalette[i], targetPalette[i], blendAmount, /*gradientProtection ? blendDirections[i] :*/ SHORTEST_HUES);
-      }
-    }
-    else {
-      // Blending just finished
-      memcpy(palette, targetPalette, sizeof(CHSV)*PALETTE_SIZE);
-      target = (target + 1) % NUM_PALETTES;
-      memcpy(targetPalette, allPalettes[target], sizeof(CHSV)*PALETTE_SIZE);
-      memcpy(oldPalette, palette, sizeof(CHSV)*PALETTE_SIZE);
-      lastSwitchTime = curTime;
-    }
+/// Logic
+void PaletteManager::Update() {
+  if(*curTime - lastSwitchTime >= pauseLength) {
+	uint32_t transitionTime = *curTime - lastSwitchTime - pauseLength;
+	if(transitionTime < walkLength) {
+	  uint8_t blendAmount = 255 * transitionTime / walkLength;
+	  for(uint8_t i = 0; i < PALETTE_SIZE; i++) {
+		palette[i] = blend(oldPalette[i], allPalettes[target][i], blendAmount, /*gradientProtection ? blendDirections[i] :*/ SHORTEST_HUES);
+	  }
+	}
+	else {
+	  // Blending just finished
+	  memcpy(palette, allPalettes[target], sizeof(CHSV)*PALETTE_SIZE);
+	  target = (target + 1) % NUM_PALETTES;
+	  memcpy(oldPalette, palette, sizeof(CHSV)*PALETTE_SIZE);
+	  lastSwitchTime = *curTime;
+	}
   }
-  // TODO: Alternatively, directly access the pointer and get rid of target
-  //targetPalette += sizeof(CHSV);
+  // todo: Alternatively, directly access the pointer and get rid of target
+  //targetPalette += sizeof(CHSV)*PALETTE_SIZE;
   //if(targetPalette == allPalettes[sizeof(CHSV)*NUM_PALETTES]) { targetPalette -= sizeof(CHSV)*NUM_PALETTES; }
-
-  #ifdef USE_TEST_PALETTE
-    memcpy(palette, test_palette, sizeof(CHSV)*PALETTE_SIZE);
-    memcpy(oldPalette, test_palette, sizeof(CHSV)*PALETTE_SIZE);
-    memcpy(targetPalette, test_palette, sizeof(CHSV)*PALETTE_SIZE);
-  #endif
 }
 
-void PaletteManager::NextPalette(uint32_t curTime) {
-  memcpy(oldPalette, targetPalette, sizeof(CHSV)*PALETTE_SIZE);
-  memcpy(palette, targetPalette, sizeof(CHSV)*PALETTE_SIZE);
+void PaletteManager::NextPalette() {
+  memcpy(oldPalette, allPalettes[target], sizeof(CHSV)*PALETTE_SIZE);
+  memcpy(palette, allPalettes[target], sizeof(CHSV)*PALETTE_SIZE);
   target = (target + 1) % NUM_PALETTES;
-  memcpy(targetPalette, allPalettes[target], sizeof(CHSV)*PALETTE_SIZE);
-  lastSwitchTime = curTime;
+  lastSwitchTime = *curTime;
 }
 
--- PaletteManager.h
+++ PaletteManager.h
@@ -5,29 +5,33 @@
 
 class PaletteManager {
   public:
-    CHSV palette[PALETTE_SIZE];
-
+	/// Fields
+	CHSV palette[PALETTE_SIZE];
+	
+	/// Object management
     PaletteManager(); 
-    void Init(uint32_t intialWalkLength, uint32_t intialPauseLength, uint32_t curTime);
-    void Update(uint32_t curTime);
-    void NextPalette(uint32_t curTime);
+    void Init(uint32_t* _curTime, uint32_t intialWalkLength, uint32_t intialPauseLength, uint8_t initialPalette);
     void SkipTime(uint32_t amount);
-    
-    uint8_t GetTarget();
-    void SetTarget(uint8_t newTarget, uint32_t curTime);
-    
+	
+    /// Accessors
+	uint8_t GetTarget();
+    void SetTarget(uint8_t newTarget);
     uint32_t GetWalkLength();
-    void SetWalkLength(uint32_t newWalkLength, uint32_t curTime);
-    
+    void SetWalkLength(uint32_t newWalkLength);
     uint32_t GetPauseLength();
-    void SetPauseLength(uint32_t newPauseLength, uint32_t curTime);
+    void SetPauseLength(uint32_t newPauseLength);
+
+	/// Logic
+    void Update();
+    void NextPalette();
+
 
   private:
+	uint32_t* curTime;
     uint8_t target;
     uint32_t walkLength;
     uint32_t pauseLength;
     CHSV oldPalette[PALETTE_SIZE];
-    CHSV targetPalette[PALETTE_SIZE];
     uint32_t lastSwitchTime;
 };
 #endif
